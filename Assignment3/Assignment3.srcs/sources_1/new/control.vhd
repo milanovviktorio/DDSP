6eed28379c39e12a3f920e5fe59ab69d64cb1e79
milanovviktorio <vikiplayz28@gmail.com>
2026-04-03 15:15:19 +0200

    control unit work started

---
+++ Assignment3/Assignment3.srcs/sources_1/new/control.vhd
@@ -0,0 +1,68 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/03/2026 02:59:41 PM
+-- Design Name: 
+-- Module Name: control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity control is
+    Generic (N : integer := 50000);
+    Port (
+        clkin       : in  STD_LOGIC;
+        clkout       : out  STD_LOGIC;
+        reset     : in  STD_LOGIC
+    );
+end control;
+
+architecture Behavioral of control is
+signal counter_divider : integer range 0 to N-1 := 0;
+signal counter_control : unsigned(5 downto 0) := (others => '0');
+signal clkdiv : STD_LOGIC := '0';
+begin
+    process (clkin, reset)
+    begin   
+        if reset = '1' then
+            counter_control <= (others => '0');
+            clkdiv <= '0';
+        elsif rising_edge(clkin) then
+            if counter_divider = (N/2-1) then
+                clkdiv <= not clkdiv;
+                counter_divider <= 0;
+                counter_control <= counter_control + 1;
+            else    
+                counter_divider <= counter_divider + 1;
+            end if;
+        end if;
+    end process;
+    
+    clkout <= clkdiv;
+
+end Behavioral;
